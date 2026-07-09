1d186d7ad03d7ae82fa4ec6687972584595dfc4a
AR3xAnish <anishsnilajkar@gmail.com>
2026-07-09 13:14:51 +0530

    server connection

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://freelance-os.vercel.app',
+    process.env.CLIENT_URL,
     /\.vercel\.app$/
   ],
   credentials: true
